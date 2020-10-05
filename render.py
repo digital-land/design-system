97f45e907d8a377b89854969b7b8c7bd2df0bc04
Colm Britton <colmjude@gmail.com>
2020-10-05 12:20:57 +0100

    Render traverses component directory looking for examples and documentation to render as pages

--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import glob
 import jinja2
 
 import codecs
@@ -69,6 +70,61 @@ def markdown_compile(s):
     return html
 
 
+component_dir = "frontend/digital_land_frontend/templates/components"
+dist_component_dir = "components"
+
+
+def get_components():
+    components = []
+    component_dirs = [d[0] for d in os.walk(component_dir)]
+    for c in component_dirs:
+        components.append(c.split("/")[-1])
+
+    # don't need to include component dir
+    if "components" in components:
+        components.remove("components")
+    return components
+
+
+def render_example_pages(components):
+    for c in components:
+        for file in glob.glob(f"{component_dir}/{c}/*.html"):
+            # don't want to render pages for the macro files
+            if not "macro.html" in file:
+                n = os.path.basename(file)
+                render(
+                    f"{dist_component_dir}/{c}/{n}",
+                    example_template,
+                    partial_name=f"digital-land-frontend/components/{c}/{n}",
+                )
+
+
+def read_markdown_file(p):
+    _content = codecs.open(
+        p,
+        mode="r",
+    )
+    return _content.read()
+
+
+def render_component_doc_pages(components):
+    for c in components:
+        documentation_path = f"{component_dir}/{c}/README.md"
+        if os.path.isfile(documentation_path):
+            markdown_content = read_markdown_file(documentation_path)
+            render(
+                f"components/{c}/index.html",
+                component_template,
+                rendered_markdown=markdown_compile(markdown_content),
+            )
+        else:
+            print(f"No documentation for {c}")
+
+
+components = get_components()
+render_example_pages(components)
+render_component_doc_pages(components)
+
 # get an example markdown file to render as a component page
 _content = codecs.open(
     "frontend/digital_land_frontend/templates/components/page-feedback/README.md",
@@ -78,35 +134,3 @@ _content_raw = _content.read()
 
 # generate the pages
 render("index.html", index_template)
-render(
-    "components/page-feedback/example.html",
-    example_template,
-    partial_name="digital-land-frontend/components/page-feedback/example.html",
-)
-render(
-    "components/page-feedback/index.html",
-    component_template,
-    rendered_markdown=markdown_compile(_content_raw),
-)
-
-# generate data-item examples
-_content = codecs.open(
-    "frontend/digital_land_frontend/templates/components/data-item/README.md",
-    mode="r",
-)
-_content_raw = _content.read()
-render(
-    "components/data-item/example.html",
-    example_template,
-    partial_name="digital-land-frontend/components/data-item/example.html",
-)
-render(
-    "components/data-item/example-inline.html",
-    example_template,
-    partial_name="digital-land-frontend/components/data-item/example-inline.html",
-)
-render(
-    "components/data-item/index.html",
-    component_template,
-    rendered_markdown=markdown_compile(_content_raw),
-)
