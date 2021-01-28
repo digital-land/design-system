4b4365cab0bc423f3519604362770eb603142ce2
Colm Britton <colmjude@gmail.com>
2021-01-28 18:23:18 +0000

    Latest build

--- docs/static/javascripts/dl-frontend.js
+++ docs/static/javascripts/dl-frontend.js
@@ -2229,6 +2229,45 @@ AppTabs.prototype.handleClick = function (event) {
   }
 };
 
+var utils = {};
+
+function camelCaseReplacer (match, s) {
+  return s.toUpperCase()
+}
+
+utils.curie_to_url_part = function (curie) {
+  return curie.replace(':', '/')
+};
+
+utils.toCamelCase = function (s) {
+  // check to see string isn't already camelCased
+  var nonWordChars = /\W/g;
+  if (s && s.match(nonWordChars)) {
+    return s.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, camelCaseReplacer)
+  }
+  return s
+};
+
+utils.truncate = function (s, len) {
+  return s.slice(0, len) + '...'
+};
+
+/**
+ * Create an organisation mapper. Maps organisation ids to names
+ * @param  {Array} orgsObj Array of organisation objs. Must contain .id and .name propterties
+ */
+utils.createOrgMapper = function (orgsObj) {
+  const mapper = {};
+  orgsObj.forEach(function (o) {
+    mapper[o.id] = o.name;
+  });
+  return mapper
+};
+
+utils.isFunction = function (x) {
+  return Object.prototype.toString.call(x) === '[object Function]'
+};
+
 function polyfill (options) {
   // polyfill for browsers without NodeList forEach method
   if (window.NodeList && !window.NodeList.prototype.forEach) {
@@ -2246,5 +2285,6 @@ exports.FilterList = FilterList;
 exports.InputCopy = InputCopy;
 exports.FilterTimelineByDate = FilterTimelineByDate;
 exports.AppTabs = AppTabs;
+exports.utils = utils;
 
 })));
