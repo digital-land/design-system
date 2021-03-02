d31259da97f0298c7746dcfd934476a9ab19ab56
Colm Britton <colmjude@gmail.com>
2021-03-02 13:28:47 +0000

    Block out template pages

--- bin/jinja_setup.py
+++ bin/jinja_setup.py
@@ -3,9 +3,13 @@
 import jinja2
 
 from digital_land_frontend.filters import (
+    group_id_to_name_filter,
     get_jinja_template_raw,
     organisation_id_to_name_filter,
     is_list,
+    strip_slug,
+    is_historical,
+    contains_historical,
 )
 
 
@@ -33,6 +37,10 @@ def setup_jinja():
     env.filters["raw_jinja"] = get_jinja_template_raw
     env.filters["organisation_id_to_name"] = organisation_id_to_name_filter
     env.filters["is_list"] = is_list
+    env.filters["group_id_to_name"] = group_id_to_name_filter
+    env.filters["clean_slug"] = strip_slug
+    env.filters["is_historical"] = is_historical
+    env.filters["contains_historical"] = contains_historical
 
     # set variables to make available to all templates
     env.globals["staticPath"] = "https://digital-land.github.io"
---
+++ documentation/digital-land/templates/index-page/index.md
@@ -0,0 +1,21 @@
+---
+contains_examples: False
+---
+# Index page templates
+
+This templates is used to generate index pages for a set of data we have collected.
+
+The template extends the [dlf-base.html](../dlf-base)
+
+{{ designSystemExample({
+"iframe": {
+    "title": "An example of a secondary action button",
+    "url": "example-basic.html",
+    "size": "xl"
+},
+"example": {
+    "path": "digital-land/templates/index-page"
+}
+}) }}
+
+### Options
